554c81d560d05b29284051c13e0f8b6a2fbb2ccb
zswDev <2624595224@qq.com>
2018-03-29 17:53:41 +0800

    up

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
-console.log('123213')
+let getThid = ()=>java.lang.Thread.currentThread().getName()
 
-xxxx@github.com
-
-1111
+for(let i=0;i<10;i++){
+        Workers(()=>{ // 线程中执行,注意异常捕获
+            //java.lang.Thread.sleep(Math.round(Math.random()*10000+5000))
+            let id = getThid()
+           postMessage(id,(v)=>{ //给主线程发消息
+                print(getThid()+':'+v)
+            })
+        })
+}
+let t = Date.now()
+setTimeout(()=>{
+  print(Date.now()-t)
+},1)
